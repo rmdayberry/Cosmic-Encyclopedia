bbb08ff8f70f7affe53f7425d7c4dc61d5a7e53d
rmdayberry <dayberryrm@gmail.com>
2024-08-31 10:05:00 -0500

    Create Navigate Back Button
    Create Navigate Back Button for user to go -1 page as needed

---
+++ .history/src/NavigateBackButton_20240831100304.jsx
@@ -0,0 +1,9 @@
+import { useNavigate } from "react-router-dom";
+
+const NavigateBackButton = () => {
+  const navigate = useNavigate();
+
+  return <button onClick={() => navigate(-1)}>Go Back</button>;
+};
+
+export default NavigateBackButton;
---
+++ src/pages/ContentPage.jsx
@@ -0,0 +1,17 @@
+import { useParams } from "react-router-dom";
+import celestialData from "../data/celestialData";
+
+export default function ContentPage() {
+  const { id } = useParams();
+  const content = celestialData[id];
+
+  if (!content) {
+    return <p>Content not found. Please choose another celestial object.</p>;
+  }
+  return (
+    <div>
+      <h2>{content.title}</h2>
+      <p>{content.description}</p>
+    </div>
+  );
+}
